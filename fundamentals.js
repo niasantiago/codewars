83f57f71638bce73d7cc0f2c63a440da42fe92e5
Nia Santiago <nia@Nias-MacBook-Air-2.local>
2023-04-06 18:43:49 -0400

    fundamentals

--- fundamentals.js
+++ fundamentals.js
@@ -7,4 +7,12 @@ var countSheep = function (num){
     murmur += i + ' sheep...';
   }
   return murmur
+}  
+
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters. 
+
+function getMiddle(s) {
+  const len = s.length;
+  const mid = Math.floor(len / 2);
+  return len % 2 === 0 ? s.substr(mid - 1, 2) : s.substr(mid, 1);
 } 
